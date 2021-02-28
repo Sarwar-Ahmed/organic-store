49964dddf2ab90c671b8bdfbf388c39ac75cb6f2
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2021-02-28 11:03:16 +0600

    order page done

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -1,11 +1,142 @@
-import React from 'react';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useContext, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { UserContext } from '../../App';
 import Header from '../Header/Header';
 
 const CheckOut = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const { register, handleSubmit, errors } = useForm();
+    const [clickedContinue, setClickedContinue] = useState(false);
+    const [cart, setCart] = useState([]);
+    const [order, setOrder] = useState([]);
+    const [total, setTotal]= useState([]);
+    const history = useHistory();
+
+    useEffect(() => {
+        fetch('http://localhost:5000/cart?email='+loggedInUser.email, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${sessionStorage.getItem('token')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            setCart(data);
+        })
+
+        fetch('http://localhost:5000/cart?email='+loggedInUser.email, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${sessionStorage.getItem('token')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            setTotal(data.map(item => item.quantity * item.price));
+        })
+    }, [cart])
+
+    const onSubmit = (data) => {
+        setClickedContinue(true);
+        const orderItems = {...cart};
+        orderItems.address = data.address;
+        orderItems.roadNo = data.roadNo;
+        orderItems.flat = data.flat;
+        orderItems.businessName = data.businessName;
+        orderItems.instructor = data.instructor;
+        orderItems.email= loggedInUser.email;
+        setOrder(orderItems);
+    }
+    const handlePlaceOrder = () => {
+        fetch('http://localhost:5000/addOrder', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+        alert("Order successfully done.");
+        history.push(`/myOrders`);
+    }
     return (
         <div>
             <Header />
-            <h1>CheckOut</h1>
+            <div className="row p-5">
+                <div className="col-md-7 p-md-5">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <h4>Edit Delivery Details</h4>
+                        <hr/>
+                        <input name="address" className="form-control bg-light" defaultValue="" ref={register({ required: true })} placeholder="Address" />
+                        {errors.address && <span className="error text-danger">Address is required</span>}
+                        <br />
+
+                        <input name="roadNo" className="form-control bg-light" defaultValue="" ref={register({ required: true })} placeholder="Road No" />
+                        {errors.roadNo && <span className="error text-danger">Road No is required</span>}
+                        <br />
+
+                        <input name="flat" className="form-control bg-light" defaultValue="" ref={register({ required: true })} placeholder="Flat, suite or floor" />
+                        {errors.flat && <span className="error text-danger">Flat, suite or floor is required</span>}
+                        <br />
+
+                        <input name="businessName" className="form-control bg-light" defaultValue="" ref={register({ required: true })} placeholder="Business Name" />
+                        {errors.businessName && <span className="error text-danger">Business Name is required</span>}
+                        <br />
+
+                        <input name="instructor" className="form-control bg-light" defaultValue="" ref={register({ required: true })} placeholder="Add delivery instructor" />
+                        {errors.instructor && <span className="error text-danger">Delivery instructor is required</span>}
+                        <br />
+
+                        <button type="submit" className="customBtn pl-3 pr-3 p-2 rounded">Save &amp; Continue</button>
+                    </form>
+                </div>
+                <div className="col-md-5 p-5 font-weight-bold" style={{textAlign: 'left'}}>
+                    <div className="p-3">
+                        <h6>From <strong>Gulshan Plaza Restaurent GPR</strong></h6>
+                        <p>Arriving in 20-30 min</p>
+                        <p>107 Rd No 8</p>
+                    </div>
+                    <div>
+                        {
+                            cart.map(cart => 
+                                <div style={{borderRadius: 20}} className="row bg-light p-2 mb-2" key={cart._id}>
+                                    <div className="col-3">
+                                        <img src={cart.image} className="img-fluid rounded-circle" alt=""/>
+                                    </div>
+                                    <div className="col-6">
+                                        <h6>{cart.title}</h6>
+                                        <h6 className="text-danger">৳ {cart.price * (cart.quantity)}</h6>
+                                        <h6 className="text-muted">Delivery free</h6>
+                                    </div>
+                                    <div className="col-3 my-auto">
+                                        <h6 className="">{cart.quantity}</h6>
+                                    </div>
+                                </div>    
+                            )
+                        }
+                    </div>
+                    <div className="p-5">
+                        <div className="d-flex">
+                            <p>Subtotal: {cart.length} item</p>
+                            <p className="ml-auto">৳ {total.reduce((previousScore, currentScore, index) => previousScore + currentScore, 0)}</p>
+                        </div>
+                        <div className="d-flex">
+                            <h6>Total</h6>
+                            <h6 className="ml-auto"><strong>৳ {total.reduce((previousScore, currentScore, index) => previousScore + currentScore, 0)}</strong></h6>
+                        </div>
+                        {
+                            clickedContinue
+                            ?<button onClick={() => handlePlaceOrder()} className="bg-success btn text-white rounded pl-5 pr-5 m-5">Place Order</button>
+                            :<button onClick={() => handlePlaceOrder()} className="bg-secondary btn text-white rounded pl-5 pr-5 m-5" disabled>Place Order</button>
+                        }
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/components/MyOrders/MyOrders.js
+++ src/components/MyOrders/MyOrders.js
@@ -1,12 +1,79 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
+import { UserContext } from '../../App';
 import Header from '../Header/Header';
 import './MyOrders.css'
 
 const MyOrders = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [order, setOrder] = useState([]);
+    const [isClicked, setIsClicked] = useState(false);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders`)
+            .then(res => res.json())
+            .then(data => {
+                setOrder(data.filter(item => item.email === loggedInUser.email));
+            })
+    }, [])
     return (
         <div>
             <Header />
-            <h1>MyOrders</h1>
+            <div className="row p-5">
+                <div className="col-md-8 p-5 bg-white">
+                    <Table responsive size="sm">
+                        <thead style={{ backgroundColor: "#F4F7FC" }}>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email ID</th>
+                                <th>Product ID</th>
+                                <th>Address</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                order.map(orderItem =>
+                                    <tr key={orderItem._id}>
+                                        <td>{orderItem.instructor}</td>
+                                        <td>{orderItem.email}</td>
+                                        <td>{orderItem._id}</td>
+                                        <td>{orderItem.roadNo}, {orderItem.address}</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </Table>
+                </div>
+                <div className="col-md-4 p-5 rounded">
+                    <div className="bg-light p-3" style={{borderRadius: 20, textAlign: "left"}}>
+                        <div>
+                            <img src="https://iili.io/2vJOzb.png" alt="" className="w-25"/>
+                        </div>
+                        <div>
+                            <h3>30 Minutes</h3>
+                            <p><small className="text-muted">Estimated delivery time</small></p>
+                        </div>
+                        {
+                            isClicked && 
+                            <div>
+                                <div className="d-flex">
+                                    <h6 className="mr-auto">Sarwar: </h6>
+                                    <p>+88017 7046 7109</p>
+                                </div>
+                                <div className="d-flex">
+                                    <h6 className="mr-auto">Mahin: </h6>
+                                    <p>+88017 3857 1086</p>
+                                </div>
+                                <div className="d-flex">
+                                    <h6 className="mr-auto">Muslam: </h6>
+                                    <p>+88017 4002 4370</p>
+                                </div>
+                            </div>
+                        }
+                        <button onClick={() => setIsClicked(true)} className="bg-danger btn text-white rounded">Contact</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
